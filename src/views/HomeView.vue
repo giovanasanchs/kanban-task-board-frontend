98b421490f9a8e134f14f53c250abfd4a726bec8
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-04 21:04:52 -0300

    :sparkles: feat: criando componente de view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <main class="home-view">
+      <KanbanBoard />
+    </main>
+  </template>
+  
+  <script setup>
+  import KanbanBoard from '../components/KanbanBoard.vue'
+  </script>
+  
+  <style scoped>
+  .home-view {
+    padding: 20px;
+  }
+  </style>
