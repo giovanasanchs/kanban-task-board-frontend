b1152c3e7ad25b5387caae93c9a995c4dfcf864e
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-04 22:40:46 -0300

    :sparkles: feat: configuração do Vue Router e aplica estrutura inicial do app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from './views/HomeView.vue'
+
+const routes = [{ path: '/', component: HomeView }]
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
