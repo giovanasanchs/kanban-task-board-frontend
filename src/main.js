c3745a99007feb75a5ce27ca081b46ffdbcc30ed
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-07 01:55:02 -0300

    :package: build: adicionando biblioteca de notificação

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './views/HomeView.vue'
@@ -11,4 +13,10 @@ const router = createRouter({
   routes,
 })
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+const app = createApp(App).use(router)
+
+app.use(Toast)
+
+app.mount('#app')
