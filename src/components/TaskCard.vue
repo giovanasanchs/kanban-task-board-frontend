43d3398cde6f7422cdf69c243c582e44af284068
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-07 15:47:33 -0300

    :sparkles: feat: adição de propriedades para as subtarefas

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,33 +1,46 @@
 <template>
-    <div class="task-card">
-      <h3 class="task-title">{{ title }}</h3>
-      <p class="task-desc">{{ description }}</p>
+  <div class="task-card">
+    <h3 class="task-title">{{ title }}</h3>
+    <p class="task-desc">{{ description }}</p>
+
+    <div v-if="subtasks.length">
+      {{ subtasks.filter((s) => s.done).length }} de
+      {{ subtasks.length }} subtarefas concluídas
     </div>
-  </template>
-  
-  <script setup>
-  defineProps(['title', 'description'])
-  </script>
-  
-  <style scoped>
-  .task-card {
-    background-color: var(--color-card);
-    padding: 20px;
-    border-radius: 8px;
-    border-left: 5px solid var(--color-secondary);
-    border-right: 1.5px solid var(--color-border);
-    border-bottom: 1.5px solid var(--color-border);
-    border-top: 1.5px solid var(--color-border);
-  }
-  
-  .task-title {
-    font-weight: bold;
-    margin: 0 0 6px;
-    color: var(--color-card-title);
-  }
-  
-  .task-desc {
-    font-size: 14px;
-    color: var(--color-card-text);
-  }
-  </style>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  description: String,
+  subtasks: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<style scoped>
+.task-card {
+  background-color: var(--color-card);
+  padding: 20px;
+  border-radius: 8px;
+  border-left: 5px solid var(--color-secondary);
+  border-right: 1.5px solid var(--color-border);
+  border-bottom: 1.5px solid var(--color-border);
+  border-top: 1.5px solid var(--color-border);
+  cursor: pointer;
+}
+
+.task-title {
+  font-weight: bold;
+  margin: 0 0 6px;
+  color: var(--color-card-title);
+}
+
+.task-desc {
+  font-size: 14px;
+  color: var(--color-card-text);
+}
+</style>
